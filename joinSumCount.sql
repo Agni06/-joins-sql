0287459ce8712788ece6b4bd43630adb6af8ed1a
JAVAJI SAI AGNI CHARAN TEJA <19311a12a6@sreenidhi.edu.in>
2024-03-29 11:50:56 +0530

    Create joinSumCount.sql

---
+++ joinSumCount.sql
@@ -0,0 +1,9 @@
+--Calculate the revenue coming from movie rentals, the number of movie rentals and the number of customers who rented a movie.
+
+SELECT 
+	SUM(m.renting_price), -- Get the revenue from movie rentals
+	COUNT(*), -- Count the number of rentals
+	COUNT(DISTINCT r.customer_id) -- Count the number of customers
+FROM renting AS r
+LEFT JOIN movies AS m
+ON r.movie_id = m.movie_id;
